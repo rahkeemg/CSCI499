966be0377896322d84b03a55e2fb5dfa28d36d09
rahkeemg <rgeorge@stetson.edu>
2016-03-12 20:23:39 -0500

    Add recursive script to retrieve all blobs from  specified commit's tree in GitPython repository

---
+++ Python/BlobRetrieval.py
@@ -0,0 +1,22 @@
+#! usr/bin/env/python
+
+import git
+
+class BlobRetrieval:
+    def __init__(self):
+        self.blobs = []
+        return None
+
+    def findFILE(self, item):
+
+        # end statement
+        if item.type == 'blob':
+            print item.abspath
+            self.blobs.append(item)
+            # return item
+        else:
+            for entry in item:
+                self.findFILE(entry)
+
+    def getBlobs(self):
+        return self.blobs
